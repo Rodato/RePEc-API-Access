07c9dd2032d7b17a6bfececba976032a20bce1d0
Daniel Otero <otero.r.daniel@gmail.com>
2018-03-01 15:33:39 -0500

    Loops corrections and better interface

--- Scripts/CallsByJEL.py
+++ Scripts/CallsByJEL.py
@@ -6,35 +6,51 @@ import json
 
 All_Data=list()
 
-code=input("Type your code access:")
+code=input("Type your access code:")
 offset=input("offset:")
 offset=int(offset)
 JEL=input("JEL code:")
+File_name=input("File name with formart (.txt, .json, .csv, etc...):")
 
 
 if __name__ == "__main__":
     url="https://api.repec.org/call.cgi"
-    payload={"code" : code, "offset" : offset, "getrecentjel" : JEL, "repecservice" : "ideas"}
+    payload={"code" : "RL2qVaps", "offset" : offset, "getrecentjel" : JEL, "repecservice" : "ideas"}
     response = rq.get(url,params=payload)
     print ("url call form:", response.url)
 
     if response.status_code==200:
-        print("Right Call")
+        print("Right call")
         content = response.content
-        if len(content)!=0:
+        first_data=content.decode("utf-8")
+        #print("Contenido tipo:", type(y))
+        y=first_data.replace("[", "")
+        y=y.replace("]", "")
+        y=y.split(",")
+        y=[i.replace('"', '') for i in y]
+        while payload["offset"]<9999999999: #This is a default value
             print("Storing data")
-            All_Data=All_Data+content
-            print("Updating call...")
+            All_Data=All_Data+y
+            print("Call update...")
             payload["offset"]=payload["offset"]+25
             print("Solicitude:", payload["offset"])
             response = rq.get(url,params=payload)
             print ("url call form:", response.url)
             content = response.content
-        else:
-            datos=open("Handles_by_JEL.json","wb")
-            datos.write(All_Data)
-            datos.close()
+            first_data=content.decode("utf-8")
+            if first_data=='[{"error":43}]': # [{"error":43}] is the API answer when not have data back
+                break
+            #print("Contenido tipo:", type(y))
+            y=first_data.replace("[", "")
+            y=y.replace("]", "")
+            y=y.split(",")
+            y=[i.replace('"', '') for i in y]
     else:
-        print("Status:", response, "please, verify your access code or url call form")
+        print("Wrong call")
+        print(response.status_code)
 
-print("¡All the data collected!")
+f = open(File_name, 'w')
+sj.dump(All_Data, f)
+f.close()
+
+print("¡All data was obtained!")
