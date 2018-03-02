bf0db89fc34f56e1a5654ed8c44b6e9c95c1cb87
Daniel Otero <otero.r.daniel@gmail.com>
2018-03-02 11:08:21 -0500

    Almost finished

--- Scripts/HandlesDictionary.py
+++ Scripts/HandlesDictionary.py
@@ -5,10 +5,13 @@ import requests as rq
 import pandas as pd
 import json
 
-file_name=input("File name with formart (.txt, .json, .csv, etc...):")
+bag=[]
+output_file_name=input("Type the output file name this way NameYouWant.csv:")
+
+input_file_name=input("File name with formart (.txt, .json, .csv, etc...):")
 code:input("Type your code access:")
 
-f=open(file_name,"r+")
+f=open(input_file_name,"r+")
 lines=f.read().split(',')
 lines = [i.replace('"', '') for i in lines]
 lines = [i.replace('[', '') for i in lines]
@@ -21,3 +24,10 @@ if __name__ == "__main__":
         payload={"code" : code, "getref" : handle, "repecservice" : "ideas"}
         response = rq.get(url,params=payload)
         print ("url call form:", response.url)
+        content= response.content
+        data=json.loads(content)
+        bag=bag+data
+
+
+full_bag=pd.DataFrame(bag)
+full_bag.to_csv(output_file_name, encoding='utf-8', index=False)
