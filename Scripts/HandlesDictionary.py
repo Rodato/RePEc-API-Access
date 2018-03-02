c0a703ade931d37e8db6270916b13dc70338d4fc
Daniel Otero <otero.r.daniel@gmail.com>
2018-03-02 14:28:03 -0500

    Adding missing variables

--- Scripts/HandlesDictionary.py
+++ Scripts/HandlesDictionary.py
@@ -6,17 +6,17 @@ import pandas as pd
 import json
 
 bag=[]
-output_file_name=input("Type the output file name this way NameYouWant.csv:")
 
 input_file_name=input("File name with format (.txt):") #If the handle file was obtained with CallsByJEL.py script, the file format should be txt
 code:input("Type your code access:")
+output_file_name=input("Type the output file name this way NameYouWant.csv:")
 
 f=open(input_file_name,"r+")
 lines=f.read().split(',')
 lines = [i.replace('"', '') for i in lines]
 lines = [i.replace('[', '') for i in lines]
 lines = [i.replace(']', '') for i in lines]
-lines = [i.replace(' ', '') for i in lines]
+handles = [i.replace(' ', '') for i in lines]
 
 if __name__ == "__main__":
     url="https://api.repec.org/call.cgi"
