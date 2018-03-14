281ae2ae137f04c74088f7bb877de98d9f1b2601
Daniel Otero <otero.r.daniel@gmail.com>
2018-03-14 08:02:01 -0500

    14-03-2018 8:01

--- Scripts/CallsItemCitation.py
+++ Scripts/CallsItemCitation.py
@@ -1,3 +1,5 @@
+# To get columns names use: print(data_c.columns.values)
+
 import pandas as pd
 import requests as rq
 import json
@@ -7,14 +9,36 @@ code=input("Type your code access:")
 output_file_name=input("Type the output file name this way NameYouWant.csv:")
 
 data_c=pd.read_csv(data)
-
-#print(data_c.columns.values) #Get the columns names
+citation_bag=[]
+citation_handle=[]
+no_citation=[]
+no_h_citation=0
+h_citation=0
 
 handlesbyjel=data_c['bibliographic.handle'].tolist()
 
 if __name__=="__main__":
     url="https://api.repec.org/call.cgi"
     for handle in handlesbyjel:
-        payload = {"code" : code, "getitemcitation" : handle, "repecservice" : "ideas"}
+        payload = {"code" : code, "getitemcitations" : handle, "repecservice" : "ideas"}
         response = rq.get(url,params=payload)
-        print ("url call form:", response.url)
+        if response.status_code==200:
+            content = response.content
+            if len(content) == 2:
+                print ("NO CITATION FOR:", response.url)
+                no_citation.append(handle)
+                no_h_citation=no_h_citation+1
+            else:
+                print ("CITATION FOR:", response.url)
+                data=json.loads(content)
+                x={"cit_handle" : handle}
+                data.append(x)
+                citation_bag=citation_bag+data
+                h_citation=h_citation+1
+
+print("Handles without cites: ",no_h_citation)
+print("Handles with cites: ",h_citation)
+
+full_bag=pd.DataFrame(citation_bag)
+full_bag=full_bag.fillna(method='bfill')[full_bag.author.notnull()]
+full_bag.to_csv("output_file.csv", encoding='utf-8', index=False)
